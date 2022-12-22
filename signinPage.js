7091b7c6e838a513b089444f6c02d2085bbeb3ac
ali elsalamouny Hub <63823488+salamouny@users.noreply.github.com>
2022-12-23 00:29:17 +0200

    Add files via upload

---
+++ signinPage.js
@@ -0,0 +1,21 @@
+let x = document.getElementById('Login');
+let y = document.getElementById('Register');
+let z = document.getElementById('btn');
+
+
+function Register() {
+
+    x.style.left = '-400px';
+    y.style.left = '50px';
+    z.style.left = '110px';
+
+}
+
+
+function Login() {
+
+    x.style.left = '50px';
+    y.style.left = '450px';
+    z.style.left = '0px';
+
+}
